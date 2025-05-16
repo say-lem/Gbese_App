1340dffaa0a6ffde3deb9359aba85065b0ab1447
say-lem <nedusalemonah@gmail.com>
2025-05-16 11:45:03 +0100

    mapper fix

--- src/utils/mappers/user.mappers.ts
+++ src/utils/mappers/user.mappers.ts
@@ -1,18 +1,22 @@
 import { IUserResponse } from '../../common/interfaces/user';
-import { IUserDocument } from '../..//modules/user-management/Models/user.model';
+import { IUserDocument } from '../../modules/user-management/Models/user.model';
 
 export function mapUserToResponse(user: IUserDocument): IUserResponse {
-    if (!user._id) throw new Error('User _id is missing');
-  
-    return {
-      userId: user._id.toString(),
-      username: user.username,
-      email: user.email,
-      phoneNumber: user.phoneNumber,
-      registrationDate: user.registrationDate,
-      baseCreditScore: user.baseCreditScore,
-      gbeseTokenBalance: user.gbeseTokenBalance,
-      role: user.role,
-      isKYCVerified: user.isKYCVerified
-    };
-  }
+  if (!user._id) throw new Error('User _id is missing');
+
+  return {
+    userId: user._id.toString(),
+    username: user.username,
+    email: user.email,
+    phoneNumber: user.phoneNumber,
+    registrationDate: user.registrationDate,
+    baseCreditScore: user.baseCreditScore ?? 0,
+    gbeseTokenBalance: user.gbeseTokenBalance ?? 0,
+    role: user.role,
+    isKYCVerified: user.isKYCVerified,
+    isEmailVerified: user.isEmailVerified,
+    walletAddress: user.walletAddress ?? '',
+    usdcBalance: user.usdcBalance ?? 0,
+    ethBalance: user.ethBalance ?? 0,
+  };
+}
