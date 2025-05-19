822773dd4357e6b378dc78945f816d3db3babeb2
say-lem <nedusalemonah@gmail.com>
2025-05-20 00:56:31 +0100

    get loan request by lenderID

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://gbese-alpha.vercel.app',
   credentials: true,
 }));
 
